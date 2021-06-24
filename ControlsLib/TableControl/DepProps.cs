f0b171ddb9a0ddf9cac3ce8aa5219b1f68a85ee4
ivan malechkin <scorpunduk@yandex.ru>
2021-06-24 18:08:42 +0300

    00 - INIT COMMIT

---
+++ ControlsLib/TableControl/DepProps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsLib
+{
+    public partial class TableControl : UserControl
+    {
+        /*  
+        Видимое на виде количество строк таблицы. Зависит от велчины по высоте 
+        отображаемой области таблицы. Будет равно частному от деления высоты
+        видимой области таблицы на заданную высоту строки +1. В случае, когда
+        все строки полностью попадают в видимую область
+        VisibleRowsNumber будет равно RowsNumber
+        */
+        public static readonly DependencyProperty VisibleRowsNumberProperty;
+        public int VisibleRowsNumber
+        {
+            get { return (int)GetValue(VisibleRowsNumberProperty); }
+            private set { SetValue(VisibleRowsNumberProperty, value); }
+        }
+
+        /*
+        Количество строк в таблице. Должно соответствовать количеству элементов
+        В случае, когда все строки полностью попадают в видимую область
+        RowsNumber будет равно VisibleRowsNumber
+        */
+        public static readonly DependencyProperty RowsNumberProperty;
+        public int RowsNumber
+        {
+            get { return (int)GetValue(RowsNumberProperty); }
+            set { SetValue(RowsNumberProperty, value); }
+        }
+    }
+}
---
+++ ControlsLib/TableControl/DepPropsRegister.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsLib
+{
+    public partial class TableControl : UserControl
+    {
+        static TableControl()
+        {
+            VisibleRowsNumberProperty =
+                SetIntegerDependencyProperty(
+                    nameof(VisibleRowsNumber),
+                    typeof(TableControl),
+                    null);
+
+            RowsNumberProperty =
+                SetIntegerDependencyProperty(
+                    nameof(RowsNumber),
+                    typeof(TableControl),
+                    null);
+        }
+
+        /* Регистрации свойства зависимости типа Int32        
+        Для свойств 
+        - RowsNumberProperty
+        - VisibleRowsNumberProperty
+        */
+        public static DependencyProperty SetIntegerDependencyProperty
+            (
+            string propName, 
+            Type ownerType, 
+            PropertyChangedCallback changedCallback
+            )
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.DefaultValue = 0;
+            metadata.AffectsMeasure = true;
+            metadata.DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            metadata.PropertyChangedCallback += changedCallback;
+            metadata.IsNotDataBindable = false;
+            metadata.BindsTwoWayByDefault = true;
+
+            return DependencyProperty.Register(propName, typeof(int), ownerType, metadata);
+        }
+    }
+}
